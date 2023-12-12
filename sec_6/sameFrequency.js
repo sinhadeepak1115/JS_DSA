56ff0db7b42271d2c37269a00962704a791d2463
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2023-12-13 04:21:57 +0530

    lets check

--- sec_6/sameFrequency.js
+++ sec_6/sameFrequency.js
@@ -37,4 +37,4 @@ function sameFrequency(num1, num2){
     // good luck. Add any arguments you deem necessary.
   }
 
-console.log(sameFrequency(12132,17808932))
+console.log(sameFrequency(12132,178032))
