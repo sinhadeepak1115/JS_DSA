dec99332becad3321f5f4c8a821d570b5f3da1a8
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2023-12-22 02:34:30 +0530

    back to DSA

---
+++ sec_9-hardProblems/capitalizeFirst.js
@@ -0,0 +1,15 @@
+//Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
+//
+//capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+
+
+function capitalizeFirst (arr) {
+  if (arr.lenght === 1){
+      return [arr[0].toUpperCase()];
+    }
+    let res = capitalizeFirst(arr.slice(0,-1));
+    res.push(arr.slice(arr.slice(array.length-1)[0].toUpperCase()));
+    return res;
+
+}
+
